e584e843a53c151ab9ed1a308f860bb44f266499
inusha-thathsara <inusha.thathsara@gmail.com>
2026-03-21 09:48:25 +0530

    Update flights.c and flights.h

--- flights.h
+++ flights.h
@@ -4,18 +4,21 @@
 #include "types.h"
 
 // ============================================================
-//  MEMBER 2 — FLIGHT OPERATIONS
+//  MEMBER 2 — FLIGHT OPERATIONS (SINGLY LINKED LIST)
 // ============================================================
 
-extern FlightNode* flightHead;
-
-int  flightExists(const char* fn);
-void addFlight();
-void removeFlight();
-void searchFlight();
-void displayAllFlights();
-void updateFlightStatus();
-void countFlights();
-void flightMenu();
+void initFlightList(struct FlightList* list);
+struct FlightNode* createFlightNode(char* flightNumber, char* origin,
+                                    char* destination, char* departureTime,
+                                    char* arrivalTime, char* gate,
+                                    char* aircraft, int totalSeats);
+int  flightExists(struct FlightList* list, char* fn);
+void addFlight(struct FlightList* list);
+void removeFlight(struct FlightList* list);
+void searchFlight(struct FlightList* list);
+void displayAllFlights(struct FlightList* list);
+void updateFlightStatus(struct FlightList* list);
+void countFlights(struct FlightList* list);
+void flightMenu(struct FlightList* list);
 
 #endif
